287549b42e036f5e9e0c2eb95b115e40c0e9f86b
aster <137767097+aster-void@users.noreply.github.com>
2025-09-08 12:47:38 +0900

    feat/i18n: remove paraglide temporarily

--- packages/client/src/hooks.server.ts
+++ packages/client/src/hooks.server.ts
@@ -2,18 +2,18 @@ import { createConvexAuthHooks } from "@mmailaender/convex-auth-svelte/sveltekit
 import type { Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 import { PUBLIC_CONVEX_URL } from "$lib/env";
-import { paraglideMiddleware } from "$lib/paraglide/server";
+// import { paraglideMiddleware } from "$lib/paraglide/server";
 
-const handleParaglide: Handle = ({ event, resolve }) =>
-  paraglideMiddleware(event.request, ({ request, locale }) => {
-    event.request = request;
+// const handleParaglide: Handle = ({ event, resolve }) =>
+//   paraglideMiddleware(event.request, ({ request, locale }) => {
+//     event.request = request;
 
-    return resolve(event, {
-      transformPageChunk: ({ html }) =>
-        html.replace("%paraglide.lang%", locale),
-    });
-  });
+//     return resolve(event, {
+//       transformPageChunk: ({ html }) =>
+//         html.replace("%paraglide.lang%", locale),
+//     });
+//   });
 
 const { handleAuth } = createConvexAuthHooks({ convexUrl: PUBLIC_CONVEX_URL });
 
-export const handle: Handle = sequence(handleParaglide, handleAuth);
+export const handle: Handle = sequence(handleAuth);
